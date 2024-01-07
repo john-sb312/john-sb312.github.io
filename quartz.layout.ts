6251d0686127130c8d6e67f55716a7277a1f18af
Long Nguyen <nguyen.vulong@protonmail.com>
2024-01-07 12:21:07 +0000

    Update quartz.layout.ts

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      GitHub: "https://github.com/john-sb312/mission",
+      "Feed": "https://john-sb312.github.io/mission/index.xml",
     },
   }),
 }
